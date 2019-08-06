1b5c9827905cc29dff7408b714d7fd51b588f40e
Charles Ren <17620463399@163.com>
2019-08-06 23:30:20 +0100

    experiment1 reproduce
    finish

---
+++ experiments/experiment3/code_ros2/minimal_publisher/examples_rclpy_minimal_publisher/throughput_server.py
@@ -0,0 +1,25 @@
+import socket
+
+
+def main(args=None):
+    BUFSIZE = 1024
+    # server address
+    ip_port = ('192.168.1.2', 9999)
+    # ip_port = ('127.0.0.1', 9999)
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  #
+    server.bind(ip_port)
+
+    print("server start!!!")
+    while True:
+        # receive data
+        data, client_addr = server.recvfrom(BUFSIZE)
+        print('server receive data: ', data)
+
+        server.sendto(data, client_addr)
+
+    server.close()
+
+
+if __name__ == '__main__':
+    # Runs a talker node when this script is run directly (not through an entrypoint)
+    main()
